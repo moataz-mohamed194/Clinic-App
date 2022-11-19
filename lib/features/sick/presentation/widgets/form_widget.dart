d31cc0e2d9db1b688c87e3b68da31a41c1157cdc
moataz-mohamed194 <mohamed.moataz194@gmail.com>
2022-11-19 03:52:26 +0200

    add the date and type of statement when booking time with doctor

--- lib/features/sick/presentation/widgets/form_widget.dart
+++ lib/features/sick/presentation/widgets/form_widget.dart
@@ -1,3 +1,5 @@
+import 'package:cool_dropdown/cool_dropdown.dart';
+import 'package:date_time_picker/date_time_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -10,6 +12,14 @@ class FormSickWidget extends StatelessWidget{
   final _formKey = GlobalKey<FormState>();
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _phoneNumberController = TextEditingController();
+  final TextEditingController _dateController = TextEditingController(text: DateTime.now().toString());
+  final TextEditingController _typeOfStatementController = TextEditingController(text: DateTime.now().toString());
+
+  final List dropdownItemList = [
+    {'label': 'Consultation', 'value': 'A'}, // label is required and unique
+    {'label': 'Statement', 'value': 'B'}
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Form(
@@ -36,11 +46,49 @@ class FormSickWidget extends StatelessWidget{
               validator: (val) => val!.isEmpty ? 'must add the phone number' :
                 val.length>11 || val.length<11?'your number not right': null,
               decoration: InputDecoration(hintText: 'phone number'),
-              minLines: 2,
-              maxLines: 2,
+
             ),
           ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            child:DateTimePicker(
+              type: DateTimePickerType.date,
+              dateMask: 'yyyy-MM-dd',
+              controller: _dateController,
+              firstDate: DateTime(2000),
+              decoration:InputDecoration(
+                  hintText:'Date of Statement'),
+              dateLabelText:'Choose the date',
+              lastDate: DateTime(2100),
+              onChanged: (value) {
+                _dateController.text = '$value'.toString();
+              },
+              validator: (val) {
+                if(val == null){
+                  return 'must choose date';
+                }
+                return null;
+              },
+            )
+          ),
+          CoolDropdown(
+            dropdownList: dropdownItemList,
+            onChange: (val) {
+              _typeOfStatementController.text = val['value'];
+            },
+            resultBD: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(5),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black,
+                ),
+              ],
+            ),
+            resultWidth:MediaQuery.of(context).size.width-60,
+            placeholder: 'Type of Statement',
 
+          ),
           ElevatedButton.icon(
               onPressed:()=> validateFormThenUpdateOrAddPost(context),
               icon: Icon(Icons.add),
@@ -60,8 +108,10 @@ class FormSickWidget extends StatelessWidget{
             id: 0,
             name: _titleController.text.toString(),
             phoneNumber: int.parse(_phoneNumberController.text),
-            typeOfStatment: 'A',
+            typeOfStatment: _typeOfStatementController.text.toString(),
+            time:_dateController.text.toString()
         );
+        print(sick);
         BlocProvider.of<AddUpdateGetSickBloc>(context)
             .add(AddSickEvent(sick: sick));
       }
