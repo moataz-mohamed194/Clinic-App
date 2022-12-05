a5ac3c07ed822babb099868ecfd7917ada8cfad1
moataz-mohamed194 <mohamed.moataz194@gmail.com>
2022-12-05 17:58:02 +0200

    remove some packages no use

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,13 +5,20 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:clinic/core/StrogeData/hive.dart';
+import 'package:clinic/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hive/hive.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     // await tester.pumpWidget(MyApp());
+    var tasksBox = await Hive.openBox<Person>('user');
+    Person? loggedData = tasksBox.get(0);
+
+    await tester.pumpWidget(MyApp(loggedData: loggedData));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
